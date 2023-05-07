2d2e137bf3aa66272bf1b90d6b88d575b5d44757
Meehyun Byeon <bmh2038@naver.com>
2023-05-07 17:27:41 +0900

    MH_branch-test_1

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const User = require("../models/user");
 
 const router = express.Router();
+console.log("나는 미현이다!!! 우하하!!!");
 
 // router.get("/", async (req, res, next) => {
 //   try {
