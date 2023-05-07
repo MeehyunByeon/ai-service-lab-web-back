c3fcb0a8efa72993501ac51ba5608b5d296ed723
안병준 <dksqodwns@gmail.com>
2023-05-07 18:59:26 +0900

    커밋 완료

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,8 @@ const User = require("../models/user");
 
 const router = express.Router();
 
+console.log("안녕하세요.");
+
 // router.get("/", async (req, res, next) => {
 //   try {
 //     const users = await User.findAll();
